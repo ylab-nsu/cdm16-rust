e8f646a239b3b371f2cf6a003b26be1406a27bb7
aelsi2 <eliseevandrey2005@gmail.com>
2026-04-11 20:51:34 +0700

    Add update feature to install script

--- cdm-install.sh
+++ cdm-install.sh
@@ -4,10 +4,11 @@ set -e
 
 print_help() {
     echo "CDM-16 Rust toolchain install script"
-    echo "Usage: $0 [-d DIRECTORY] [-v RELEASE] [-t NAME] [-s] [-h]"
+    echo "Usage: $0 [-d DIRECTORY] [-t NAME] [-v RELEASE] [-u] [-s] [-h]"
     echo " -d DIRECTORY : The directory to install the Rust toolchain to. Default: ~/.rust-cdm"
-    echo " -v VERSION   : The name of the release to download. Default: cdm-nightly"
     echo " -t NAME      : The name of the toolchain to use in rustup. Default: cdm"
+    echo " -v VERSION   : The name of the release to download. Default: cdm-nightly"
+    echo " -u           : Update the toolchain if it already exists."
     echo " -s           : Script mode. Don't ask interactive questions."
     echo " -h           : Display this help message."
 }
@@ -73,20 +74,58 @@ check_tools() {
     return $result
 }
 
+check_existing() {
+    if ! [ -f "$1/.cdm-rust" ]; then
+        return 1
+    fi
+    if ! [ -f "$1/cdm-uninstall.sh" ] || ! [ -x "$1/cdm-uninstall.sh" ]; then
+        return 1
+    fi
+    . "$1/.cdm-rust"
+    if [ $# -ge 2 ]; then
+        eval "$2=\"\$_version\""
+    fi
+    if [ $# -ge 3 ]; then
+        eval "$3=\"\$_toolchain_name\""
+    fi
+}
+
 check_install_dir() {
     if [ -f "$1" ]; then
         echo "Install directory is a file: $1" >&2
         echo "Please, specify a different install directory or remove the file." >&2
         return 1
     fi
-    if [ -d "$1" ]; then
-        ls_output=$(ls -A "$1")
-        if [ -n "$ls_output" ]; then
-            echo "Install directory is not empty: $1" >&2
-            echo "Please, specify a different install directory or remove the contents of the directory." >&2
-            return 1
+    if ! [ -d "$1" ]; then
+        eval "$2=0"
+        return 0
+    fi
+    if check_existing "$1" _version _toolchain_name; then
+        eval "update_val=\"\$$2\""
+        if [ -z "$update_val" ]; then
+            echo "Directory $1 already contains toolchain version $_version"
+            if ! prompt_confirm "Would you like to update the existing toolchain? (Y/n)"; then
+                return 1
+            fi
+            update_val=1
+            eval "$2=\"\$update_val\""
+        fi
+        if [ "$update_val" -ne 0 ]; then
+            eval "toolchain_name_val=\"\$$3\""
+            if [ -z "$toolchain_name_val" ]; then
+                eval "$3=\"\$_toolchain_name\""
+            fi
+            return 0
         fi
     fi
+    ls_output=$(ls -A "$1")
+    if [ -n "$ls_output" ]; then
+        echo "Install directory is not empty: $1" >&2
+        echo "Please, specify a different install directory or remove the contents of the directory." >&2
+        return 1
+    fi
+    eval "$2=0"
+    return 0
 }
 
 check_rustup_toolchain() {
@@ -107,6 +146,9 @@ check_rustup_toolchain() {
           return 1
           ;;
     esac
+    if [ "$1" = "$2" ]; then
+        return 0
+    fi
     toolchains=$(rustup toolchain list)
     for toolchain in $(echo "$toolchains" | awk '{print $1}'); do
         if [ "$toolchain" = "$1" ]; then
@@ -118,15 +160,19 @@ check_rustup_toolchain() {
 }
 
 prompt_input() {
+    prompt_msg="$1"
+    prompt_out="$2"
+    prompt_cmd="$3"
+    shift 3
     while true; do
-        printf "%s " "$3"
+        printf "%s " "$prompt_msg"
         read resp </dev/tty
         resp=$(expand_tilde "$resp") || continue
         if [ -z "$resp" ]; then
-            eval "resp=\"\$$1\""
+            eval "resp=\"\$$prompt_out\""
         fi
-        if $2 $resp; then
-            eval "$1=\"\$resp\""
+        if "$prompt_cmd" "$resp" "$@"; then
+            eval "$prompt_out=\"\$resp\""
             return 0
         fi
     done
@@ -135,8 +181,8 @@ prompt_input() {
 prompt_confirm() {
     while true; do
         printf "%s " "$1"
-        read resp </dev/tty
-        case "$resp" in
+        read confirm_resp </dev/tty
+        case "$confirm_resp" in
             [Yy]* | '')
                 return 0
                 ;;
@@ -155,12 +201,14 @@ version=
 triple=
 toolchain_name=
 no_ask=
+update=
 
-while getopts "d:v:t:sh" opt; do
+while getopts "d:v:t:ush" opt; do
     case $opt in
         d) install_dir="$OPTARG" ;;
         v) version="$OPTARG" ;;
         t) toolchain_name="$OPTARG" ;;
+        u) update=1 ;;
         s) no_ask=1 ;;
         h)
             print_help
@@ -180,45 +228,50 @@ if [ $# -gt 0 ]; then
     exit 1
 fi
 
-triple=$(get_triple)
-check_tools rustup curl bash
+triple="$(get_triple)"
+no_ask="${no_ask:-0}"
+install_dir="${install_dir:-$(get_default_install_dir $no_ask)}"
+version="${version:-cdm-nightly}"
 
-if [ -z "$no_ask" ]; then
-    no_ask=0
-fi
 if [ "$no_ask" -eq 0 ] && ! true 2>/dev/null >/dev/tty; then
     echo "Cannot run in interactive mode without a controlling terminal." >&2
     exit 1
 fi
 
-if [ -z "$install_dir" ]; then
-    install_dir=$(get_default_install_dir $no_ask)
-fi
-if [ -z "$version" ]; then
-    version=cdm-nightly
-fi
-if [ -z "$toolchain_name" ]; then
-    toolchain_name=cdm
-fi
+check_tools rustup curl bash
 
 if [ "$no_ask" -eq 0 ]; then
-    prompt_input version true "Enter release name ($version):"
-    prompt_input install_dir check_install_dir "Enter install directory ($install_dir):"
-    prompt_input toolchain_name check_rustup_toolchain "Enter toolchain name ($toolchain_name):"
+    prompt_input "Enter release name ($version):" version true
+    prompt_input "Enter install directory ($install_dir):" install_dir check_install_dir update upd_toolchain_name
+    toolchain_name="${toolchain_name:-$upd_toolchain_name}"
+    toolchain_name="${toolchain_name:-cdm}"
+    prompt_input "Enter toolchain name ($toolchain_name):" toolchain_name check_rustup_toolchain "$upd_toolchain_name"
     echo
+else
+    update="${update:-0}"
+    check_install_dir "$install_dir" update upd_toolchain_name
+    toolchain_name="${toolchain_name:-$upd_toolchain_name}"
+    toolchain_name="${toolchain_name:-cdm}"
+    check_rustup_toolchain "$toolchain_name" "$upd_toolchain_name"
 fi
-
-check_install_dir "$install_dir"
-check_rustup_toolchain "$toolchain_name"
 
 echo "Host platform:            $triple"
 echo "Toolchain version:        $version"
 echo "Install directory:        $install_dir"
 echo "Rustup toolchain name:    $toolchain_name"
+if [ "$update" -ne 0 ]; then
+    echo
+    echo "The existing toolchain $install_dir will be uninstalled."
+fi
 echo
 if [ "$no_ask" -eq 0 ] && ! prompt_confirm "Proceed with installation? (Y/n)"; then
     echo "Installation canceled."
     exit 0
+fi
+
+if [ "$update" -ne 0 ]; then
+    echo "Uninstalling existing toolchain..."
+    "$install_dir/cdm-uninstall.sh" -s
 fi
 
 download_dir=$(mktemp -d "${TMPDIR:-/tmp}/rust-cdm-download.XXXXXX")
@@ -259,13 +312,13 @@ rustup toolchain link "$toolchain_name" "$install_dir"
 echo "Installing nightly toolchain..."
 rustup toolchain install nightly
 
-toolchain_info="$install_dir/.cdm-toolchain"
+toolchain_info="$install_dir/.cdm-rust"
 uninstall_script="$install_dir/cdm-uninstall.sh"
 
 echo "Writing toolchain info..."
 cat >"$toolchain_info" <<EOF
-version="$version"
-toolchain_name="$toolchain_name"
+_version="$version"
+_toolchain_name="$toolchain_name"
 EOF
 
 echo "Writing uninstall script..."
