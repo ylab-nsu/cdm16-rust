25290ddaebefdeda5ed8ece7362cd3ad20d0d44b
aelsi2 <eliseevandrey2005@gmail.com>
2026-05-03 01:47:37 +0700

    Use compiler-allocated registers in CDM mem builtins

--- library/compiler-builtins/compiler-builtins/src/mem/cdm.rs
+++ library/compiler-builtins/compiler-builtins/src/mem/cdm.rs
@@ -4,92 +4,91 @@ use core::ffi::c_int;
 // Smaller and simpler implementations for CDM-16.
 
 #[inline(always)]
-pub unsafe fn copy_forward(dest: *mut u8, src: *const u8, count: usize) {
+pub unsafe fn copy_forward(mut dest: *mut u8, mut src: *const u8, mut count: usize) {
     asm!(
-        "tst r2",
+        "tst {cnt}",
         "bz 1f",
         "0:",
-        "ldb r1, r3",
-        "stb r0, r3",
-        "inc r0",
-        "inc r1",
-        "dec r2",
+        "ldb {src}, {tmp}",
+        "stb {dst}, {tmp}",
+        "inc {src}",
+        "inc {dst}",
+        "dec {cnt}",
         "bnz 0b",
         "1:",
-        in("r0") dest,
-        in("r1") src,
-        in("r2") count,
-        clobber_abi("C"),
+        dst = inout(reg) dest,
+        src = inout(reg) src,
+        cnt = inout(reg) count,
+        tmp = out(reg) _,
         options(nostack),
     );
 }
 
 #[inline(always)]
-pub unsafe fn copy_backward(dest: *mut u8, src: *const u8, count: usize) {
+pub unsafe fn copy_backward(mut dest: *mut u8, mut src: *const u8, mut count: usize) {
     asm!(
-        "tst r2",
+        "tst {cnt}",
         "bz 1f",
-        "add r0, r2, r0",
-        "add r1, r2, r1",
-        "dec r0",
-        "dec r1",
+        "add {dst}, {cnt}, {dst}",
+        "add {src}, {cnt}, {src}",
+        "dec {dst}",
+        "dec {src}",
         "0:",
-        "ldb r1, r3",
-        "stb r0, r3",
-        "dec r0",
-        "dec r1",
-        "dec r2",
+        "ldb {src}, {tmp}",
+        "stb {dst}, {tmp}",
+        "dec {dst}",
+        "dec {src}",
+        "dec {cnt}",
         "bnz 0b",
         "1:",
-        in("r0") dest,
-        in("r1") src,
-        in("r2") count,
-        clobber_abi("C"),
+        dst = inout(reg) dest,
+        src = inout(reg) src,
+        cnt = inout(reg) count,
+        tmp = out(reg) _,
         options(nostack),
     );
 }
 
 #[inline(always)]
-pub unsafe fn set_bytes(s: *mut u8, c: u8, n: usize) {
+pub unsafe fn set_bytes(mut s: *mut u8, c: u8, mut n: usize) {
     asm!(
-        "tst r2",
+        "tst {cnt}",
         "bz 1f",
         "0:",
-        "stb r0, r1",
-        "inc r0",
-        "dec r2",
+        "stb {dst}, {val}",
+        "inc {dst}",
+        "dec {cnt}",
         "bnz 0b",
         "1:",
-        in("r0") s,
-        in("r1") c,
-        in("r2") n,
-        clobber_abi("C"),
+        dst = inout(reg) s,
+        val = in(reg) c,
+        cnt = inout(reg) n,
         options(nostack),
     );
 }
 
 #[inline(always)]
-pub unsafe fn compare_bytes(s1: *const u8, s2: *const u8, n: usize) -> c_int {
+pub unsafe fn compare_bytes(mut s1: *const u8, mut s2: *const u8, mut n: usize) -> c_int {
     let result : c_int;
     asm!(
-        "ldi r4, 0",
-        "tst r2",
+        "ldi {val2}, 0",
+        "tst {cnt}",
         "bz 1f",
         "0:",
-        "ldb r0, r3",
-        "ldb r1, r4",
-        "sub r3, r4, r4",
+        "ldb {ptr1}, {val1}",
+        "ldb {ptr2}, {val2}",
+        "sub {val1}, {val2}, {val2}",
         "bnz 1f",
-        "inc r0",
-        "inc r1",
-        "dec r2",
+        "inc {ptr1}",
+        "inc {ptr2}",
+        "dec {cnt}",
         "bnz 0b",
         "1:",
-        in("r0") s1,
-        in("r1") s2,
-        in("r2") n,
-        out("r4") result,
-        clobber_abi("C"),
+        ptr1 = inout(reg) s1,
+        ptr2 = inout(reg) s2,
+        cnt = inout(reg) n,
+        val1 = out(reg) _,
+        val2 = out(reg) result,
         options(readonly, nostack),
     );
     result
@@ -99,18 +98,19 @@ pub unsafe fn compare_bytes(s1: *const u8, s2: *const u8, n: usize) -> c_int {
 pub unsafe fn c_string_length(mut s: *const core::ffi::c_char) -> usize {
     let len : usize;
     asm!(
-        "move r0, r1",
+        "move {ptr}, {start}",
         "br 1f",
         "0:",
-        "inc r0",
+        "inc {ptr}",
         "1:",
-        "ldb r0, r2",
-        "tst r2",
+        "ldb {ptr}, {tmp}",
+        "tst {tmp}",
         "bnz 0b",
-        "sub r0, r1, r1",
-        in("r0") s,
-        out("r1") len,
-        clobber_abi("C"),
+        "sub {ptr}, {start}, {res}",
+        ptr = inout(reg) s,
+        start = out(reg) _,
+        tmp = out(reg) _,
+        res = lateout(reg) len,
         options(readonly, nostack),
     );
     len
